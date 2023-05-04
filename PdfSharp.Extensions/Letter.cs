b21ac48b9bdf3c6f50c2e699a48a95bb837d4d65
Jörg Nowak <kontakt@nowak-webdesign.de>
2023-05-04 08:03:20 +0200

    Add GDI support

--- PdfSharp.Extensions/Letter.cs
+++ PdfSharp.Extensions/Letter.cs
@@ -1,8 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+#if GDI
+using System.Drawing;
+#endif
 
 namespace PdfSharp.Extensions
 {
